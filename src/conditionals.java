ae082afa6e573c3f26436bf185ec29681a75ad19
Ojasvi Soni <ojasvisoni20@gmail.com>
2022-05-16 09:19:24 +0530

    conditionals

---
+++ src/conditionals.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class conditionals {
+    public static void main(String[] args){
+        int n1=10;
+        int n2=20;
+        if(n1 == n2){
+            System.out.println(n1 + " is equal to " + n2);
+        } else if(n1 > n2){
+            System.out.println(n1 + " is greater than " + n2);
+        } else{
+            System.out.println(n2 + " is greater than " + n1);
+        }
+    }
+}
+
