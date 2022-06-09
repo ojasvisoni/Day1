ae3eecbeccffb9689352a227bc0183674db2af1e
ojasvisoni <ojasvisoni20@gmail.com>
2022-06-09 15:06:34 +0530

    Reverse printing of a number

---
+++ src/Reverse_number.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Reverse_number {
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        while(n != 0){
+            int a = n%10;
+            System.out.println(a);
+            n = n/10;
+        }
+    }
+}
