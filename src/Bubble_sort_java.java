dd265ef50b345c4c309f443d797d0b39827b0e83
Ojasvi Soni <ojasvisoni20@gmail.com>
2022-07-15 12:44:06 +0530

    Bubble Sort/ Sinking Sort

---
+++ src/Bubble_sort_java.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class Bubble_sort_java {
+    public static class BubbleSort{
+        static void bubblesort(int[] arr){
+            int n = arr.length;
+            for(int i=0; i<n-1; i++){
+                for(int j=0; j<n-i-1; j++){
+                    if(arr[j] > arr[j+1]){
+                        int temp = arr[j];
+                        arr[j] = arr[j+1];
+                        arr[j+1] = temp;
+                    }
+                }
+            }
+        }
+
+        static void printArray(int[] arr){
+            int n = arr.length;
+            for(int i=0; i< n; i++){
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args){
+       int[] arr = {5, 3, 2, 7, 8, 1, 23, 15};
+       BubbleSort.printArray(arr);
+       BubbleSort.bubblesort(arr);
+       BubbleSort.printArray(arr);
+    }
+}
